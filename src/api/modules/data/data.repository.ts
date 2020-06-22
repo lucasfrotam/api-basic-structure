03157d9afe1e5f9ab702ad862a5c1651603009d2
Lucas Marinho <lucasfrotam@gmail.com>
2020-06-22 11:05:37 -0400

    Basic structure implemented

---
+++ src/api/modules/data/data.repository.ts
@@ -0,0 +1,19 @@
+import { MongoController } from '../../common/mongo';
+import { Repository } from '../../common/repository/repository';
+import { Item } from '../../common/model/item.model'
+
+export class DataRepository extends Repository<Item> {
+  constructor(mongo: MongoController) {
+    super(mongo, 'test-collection');
+  }
+
+  findItemById = async (id: string): Promise<Item> => {
+    const item = await this.findOne({ _id: id });
+    return item;
+  }
+
+  getAllData = async (): Promise<Item[]> => {
+    const data = await this.findAll();
+    return data.toArray();
+  }
+}
